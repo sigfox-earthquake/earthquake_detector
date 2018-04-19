d4e7825bceeeb7bdbced5c78882e398783f10e8d
Louis Moreau <louis.moreau@sigfox.com>
2018-04-19 13:30:52 -0700

    [feature] added basic GPS function

--- earthquake_detector.ino/earthquake_detector.ino.ino
+++ earthquake_detector.ino/earthquake_detector.ino.ino
@@ -44,13 +44,66 @@
 
 
 #define DEBUG 1
+
 // Variables
+
+   //Sigfox
 uint8_t msg[12];
-typedef struct {
+
+    //GPS
+    //Position structure, data stored as degrees, decimals minutes
+  typedef struct {
     int32_t latitude;       ///< Latitude in 1/100000 degrees, South if < 0, North otherwise
     int32_t longitude;        ///< Longitude in 1/100000 degrees, West if < 0, East otherwise
     int16_t altitude;       ///< Altitude in meters above sea level
-  } TD_GEOLOC_Position_t;
+  } GPS_Position;
+
+  //Date & Time structure
+  typedef struct {
+    uint16_t year;          ///< Year UTC
+    uint8_t seconds;        ///< Seconds 0..59
+    uint8_t minutes;        ///< Minutes 0..59
+    uint8_t hours;          ///< Hours 0..23
+    uint8_t day;            ///< Days 1.31
+    uint8_t month;          ///< Month 1..12
+  } GPS_DateTime;
+
+  // Quality structure
+  typedef struct {
+    uint16_t hdop;          ///< Horizontal Degree Of Precision, 0 .. 20 * 100
+    uint16_t hAcc;          ///< Horizontal estimated Accuracy in meters
+    uint8_t sat;            ///< Number of satellites used for fix, maximum 20
+  } GPS_Quality;
+
+  // GPS fix structure types
+  typedef enum {
+    TD_GEOLOC_NO_FIX = 0,
+    TD_GEOLOC_TIME_FIX = 1,
+    TD_GEOLOC_DATE_FIX = 2,
+    TD_GEOLOC_2D_FIX = 3,
+    TD_GEOLOC_3D_FIX = 4
+  } GPS_FixType;
+
+  //Geoloc information saved in logger */
+  typedef struct {
+    GPS_Position position;  ///< Position information
+    GPS_DateTime datetime;     ///< Date & time information
+  } GPS_Log;
+
+  // Full structure containing all updated informations
+  typedef struct {
+    GPS_DateTime datetime;    ///< Date & time information
+    GPS_Position position;    ///< Position information
+    GPS_Quality quality;      ///< Quality information
+    uint32_t speed;           ///< Speed information (in km/h)
+    GPS_FixType type;         ///< Type of fix information available
+    uint32_t duration;        ///< Fix duration in seconds
+  } GPS_Fix;
+
+
+  //Accelero
+
+  //Battery
 
 
 // Sigfox
@@ -84,27 +137,90 @@ void loop() {
   
 }
 
-void setupDevice(){
-  // This function is used to setup the device in the field.
-  // It must get the GPS information: Lat, Long, Alt and store them in memory
-  // It must sync the internal clock
-  // If the GPS acquisition went well, it must send a Sigfox message with:
-  // - The message type indicator (setup)
-  // - The battery level
-  // - The lat, long, alt
-  // If it went wrong (no GPS information for instance):
-  // - The message type indicator
-  // - The battery level
+/***************************************************************************//**
+ * @brief
+   This function is used to setup the device in the field.
+   It must get the GPS information: Lat, Long, Alt and store them in memory
+   It must sync the internal clock
+   If the GPS acquisition went well, it must send a Sigfox message with:
+   - The message type indicator (setup)
+   - The battery level
+   - The lat, long, alt
+   If it went wrong (no GPS information for instance):
+   - The message type indicator
+   - The battery level
+ *
+ * @param[in] none at the moment
+
+ ******************************************************************************/
+static void setupDevice(){
+  
 }
 
-void fixGPS(int timout){
-  // Function to get a GPS fix, with the timeout in seconds as a parameter.
-  // The timeout is maximum time for the device to get the GPS. 
-  // If the timeout value is reached, return an error value.
+/***************************************************************************//**
+ * @brief
+ *   Start the GPS and try to get a fix. If the GPS is already running then no
+ *   effect.
+ *
+ * @param[in] timeout
+ *  timeout in seconds. On timeout expiration the timeout parameter of the
+ *  callback will be set to true. No further action is being performed and
+ *  shutting down the GPS or not is left to the user application and should be
+ *  done using GPS_StopFix.
+ *
+ * @param[in] precision
+ *   Stop the GPS when fix type precision has fullfiled requirements
+ *
+ *
+ ******************************************************************************/
+void startGPSFix(uint16_t timeout, GPS_FixType precision){
   
-  unsigned long latitude, longitude, altitude;
-  int hdop, nbSat, acqTime, speed, size;
 }
+
+
+/***************************************************************************//**
+ * @brief
+ *  Power down the GPS
+ *
+ * @param[in] none at the moment
+ ******************************************************************************/
+static void StopGPSFix(){
+  
+}
+
+
+/***************************************************************************//**
+ * @brief
+ *  GPS fix callback
+ *
+ * @param[in] fix
+ *   The GPS fix data structure.
+ *
+ * @param[in] timeout
+ *   Flag that indicates whether a timeout occurred if set to true.
+ ******************************************************************************/
+static void GPSFix(GPS_Fix * fix, bool timeout){
+  StopGPSFix();
+  
+}
+
+
+
+/***************************************************************************//**
+ * @brief
+ *  Calibrate internal clock
+ *
+ * @param[in] fix
+ *   The GPS fix data structure.
+ *
+ * @param[in] timeout
+ *   Flag that indicates whether a timeout occurred if set to true.
+ ******************************************************************************/
+void calibrateClock(){
+  
+}
+
+
 
 
 //Send Sigfox Message
